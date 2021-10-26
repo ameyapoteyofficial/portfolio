95da4775e63c0fdc613625989655915c7d3e69a6
AmeyaPotey777 <42436706+ameyapoteyofficial@users.noreply.github.com>
2021-10-26 11:15:38 -0500

    image changed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/Ameya-1.png" alt="" />
+                    <img src="assets/Ameya.png" alt="" />
                 </div>
             </div>
             <div className="right">
